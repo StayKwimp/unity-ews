b7e5b35428e8ff1e7586311877c976d6cef9aeb4
Tom Mouw <tom.mouw@xs4all.nl>
2023-07-04 10:14:29 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# unity-ews
-Github voor de game die we maken voor ons ews
+# Unity EWS project
+Dit is de *private* github page die we gebruiken tijdens ons ews. Ja ik weet niet echt wat ik hier nog verder moet zeggen.
+
+## Unity editor
+De Unity editor version die voor het project in *unity-project* staat is **2021.3.25f1** (te vinden op de unity *download archive*)
---
+++ unity-project/Assets/PlayerCam.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCam : MonoBehaviour
+{
+    public float sensX;
+    public float sensY;
+
+    public Transform orientation;
+
+    float xRotation;
+    float yRotation;
+
+    private void Start(){
+        
+        // zet de mouse cursor op invisible en locked midden op het scherm
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+
+    private void Update(){
+        // get mouse input
+        float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * sensX;
+        float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sensY;
+
+        // niet vragen, i dont understan :moyai:
+        yRotation += mouseX;
+
+        xRotation -= mouseY;
+
+
+        // zorg dat xRotation binnen -90 en 90 graden blijft, zodat je niet verder dan dat omhoog en omlaag kan kijken
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        // rotate camera
+        transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
+
+        // rotate player
+        orientation.rotation = Quaternion.Euler(0, yRotation, 0);
+
+
+
+    }
+}
